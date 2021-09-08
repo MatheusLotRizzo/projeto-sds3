19becdc4bca1911391b7a578ae4fc0324a7be2a2
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2021-09-08 19:59:28 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -22,6 +22,10 @@ public class Seller {
 	@OneToMany(mappedBy = "seller")
 	private List<Sale> sales = new ArrayList();
 	
+	public Seller() {
+		
+	}
+	
 	public Seller(Long id, String name) {
 		this.id = id;
 		this.name = name;
